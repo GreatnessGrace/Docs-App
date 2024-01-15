ea541613634b1be04a9c012e87e29c56b46088d9
GreatnessGrace <graceunstoppable7@gmail.com>
2024-01-15 12:39:58 +0530

    [WIP] Card Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,10 @@ import { FaRegFileAlt } from "react-icons/fa";
 
 function Card(params) {
     return (<>
-            <div className=" rounded-[20px] w-60 h-80 bg-zinc-900/90 text-white p-5">
+            <div className="relative  rounded-[50px] w-60 h-72 bg-zinc-900/90 text-white px-5 py-101 overflow-hidden">
             <FaRegFileAlt/>
-<p className="text-xs mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eligendi, iusto eum et dolor libero culpa inventore sint natus quae aperiam vel, sit possimus at necessitatibus sequi provident ex odit.</p>
+<p className="text-xs leading-tight mt-5 font-semibold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eligendi, iusto eum et dolor libero culpa inventore sint natus quae aperiam vel, sit possimus at necessitatibus sequi provident ex odit.</p>
+<div className="footer absolute bottom-0 bg-sky-200 w-full h-10  left-0"></div>
             </div>
 
     </>)
